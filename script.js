4230040ddbdf6b9fa22df792d1c488acdf016c90
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-17 23:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	 event.preventDefault();
 var radius= document.getElementById("radius").value;
 var volume=(4/3)*Math.PI* Math.pow(radius, 3);
 document.getElementById("volume").value = volume.toFixed(2);
@@ -7,6 +8,7 @@ document.getElementById("volume").value = volume.toFixed(2);
 
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
-document.getElementById('MyForm').addEventListener('submit', volume_sphere);
-document.getElementById('submit').addEventListener('click', volume_sphere);
+window.onload = function () {
+	document.getElementById('MyForm').addEventListener('submit', volume_sphere);
+	document.getElementById('submit').addEventListener('click', volume_sphere);	
+}
